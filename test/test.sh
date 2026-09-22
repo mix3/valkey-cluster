8437f8dda9f3615ce369c2380067b33990f86cc9
mix3@ｻﾀﾃﾞｰﾅｲﾄﾌｨｰﾊﾞｰ <himachocost333@gmail.com>
2026-09-22 13:33:39 +0900

    fix test

--- test/test.sh
+++ test/test.sh
@@ -5,26 +5,42 @@ set -e
 echo "VALKEY_VERSION=${VALKEY_VERSION:-latest}"
 echo "VALKEY_VARIANT=$VALKEY_VARIANT"
 
+services=$(yq '... comments="" | .services | keys' compose.yml | grep -v test-runner- | sed 's/- //g')
+testrunner1=$(yq '... comments="" | .services | keys' compose.yml | grep test-runner-1 | sed 's/- //g')
+testrunner2=$(yq '... comments="" | .services | keys' compose.yml | grep test-runner-2 | sed 's/- //g')
+
+# init
+docker compose down -v
 docker compose build
 
-services=$(yq '.services | keys' compose.yml | sed 's/- //g')
-
+# launch
 for h in $(echo "$services")
 do
 	docker compose up -d $h
 done
+docker compose up -d $(echo $services | xargs) --wait
 
-docker compose up -d --wait
+# test-1
+for h in $(echo "$testrunner1")
+do
+	docker compose run --rm $h
+done
 
+# relaunch
 docker compose down
-
 for h in $(echo "$services" | awk 'NR%2==0' && echo "$services" | awk 'NR%2!=0')
 do
 	docker compose up -d $h
 done
+docker compose up -d $(echo $services | xargs) --wait
 
-docker compose up -d --wait
+# test-2
+for h in $(echo "$testrunner2")
+do
+	docker compose run --rm $h
+done
 
+# check moduleload
 if [ "$VALKEY_VARIANT" == "-bundle" ];  then
 	for h in $(docker-compose exec default bash -c "ls /usr/lib/valkey/*.so")
 	do
